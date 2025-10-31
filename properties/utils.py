35faed645a6a9514ee8b6c1f451eea40e5ebb42e
gerald-nyerer <geraldnyerer100@gmail.com>
2025-10-31 10:56:55 +0300

    Cache Metrics Analysis

--- properties/utils.py
+++ properties/utils.py
@@ -33,7 +33,7 @@ def get_redis_cache_metrics():
         hits = info.get("keyspace_hits", 0)
         misses = info.get("keyspace_misses", 0)
         total = hits + misses
-        hit_ratio = (hits / total) if total > 0 else 0.0
+        hit_ratio = (hits / total) if total > 0 else 0
 
         metrics = {
             "keyspace_hits": hits,
